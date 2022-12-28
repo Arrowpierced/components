bd85bdd354a7a15cdf037776614f627243ab4313
Arrowpierced <101281738+Arrowpierced@users.noreply.github.com>
2022-12-28 00:22:26 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import faker from 'faker'
+import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
+import './App.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+    
+    return (
+        <div className='ui container comments'>
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+            <CommentDetail 
+            author="Carl" 
+            timeAgo="Today at 4:30" 
+            text="This is a post"
+            avatar={faker.image.image()}
+            />
+            </ApprovalCard>
+            <ApprovalCard>
+            <CommentDetail 
+            author="Alex"
+            timeAgo="Today at 3:30" 
+            text="This is another post"
+            avatar={faker.image.image()}
+            />
+            </ApprovalCard>
+            <ApprovalCard>
+            <CommentDetail 
+            author="Jane" 
+            timeAgo="Yesterday at 2:22" 
+            text="This is a third post"
+            avatar={faker.image.image()}
+            />
+            </ApprovalCard>
+        </div>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
